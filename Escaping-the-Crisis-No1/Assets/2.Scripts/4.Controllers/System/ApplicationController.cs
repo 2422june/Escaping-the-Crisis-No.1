63e73b355172f2a3a7787d0c5866b01c98900aba
2422june <ck66661254@gmail.com>
2023-12-21 16:57:53 +0900

    without oculus

---
+++ Escaping-the-Crisis-No1/Assets/2.Scripts/4.Controllers/System/ApplicationController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//게임의 엔트리 포인트이며 게임에 전체적으로 작동할 것들을 셋팅하고 게임을 제어한다.
+
+[RequireComponent(typeof(Managers))] // 필수 컴포넌트로 지정
+public class ApplicationController : MonoBehaviour
+{
+    private void Awake()
+    {
+        Managers.Root = Util.GetOrAddComponent<Managers>(gameObject);
+        Managers.Root.Init();
+
+        EnterGame();
+    }
+
+    private void EnterGame()
+    {
+        Managers.Scene.LoadScene(Define.Scene.Main);
+    }
+}
