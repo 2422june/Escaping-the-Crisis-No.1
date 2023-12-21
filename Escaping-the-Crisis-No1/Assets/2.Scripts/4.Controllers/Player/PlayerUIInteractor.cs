54d15c8e2cf62c24f2e2d01d1fc9efdf6ec27417
2422june <ck66661254@gmail.com>
2023-12-22 01:05:05 +0900

    VR UI Interact

--- Escaping-the-Crisis-No1/Assets/2.Scripts/4.Controllers/Player/PlayerUIInteractor.cs
+++ Escaping-the-Crisis-No1/Assets/2.Scripts/4.Controllers/Player/PlayerUIInteractor.cs
@@ -7,5 +7,24 @@ public class PlayerUIInteractor : MonoBehaviour
     [SerializeField]
     private Transform _handL, _handR;
 
+    private RaycastHit _handLHit, _handRHit;
+    private Ray _handLRay, _handRRay;
+    private LineRenderer _lineR, _lineL;
+    private GameObject _targetL, _targetR;
+    private float _rayDis = 10f;
 
+    private void Start()
+    {
+        _lineR = _handR.GetComponent<LineRenderer>();
+        _lineL = _handL.GetComponent<LineRenderer>();
+
+        _handRRay.origin = _handR.position;
+        _handLRay.origin = _handL.position;
+    }
+
+    private void Update()
+    {
+        _handRRay.direction = _handR.forward;
+        _handLRay.direction = _handL.forward;
+    }
 }
